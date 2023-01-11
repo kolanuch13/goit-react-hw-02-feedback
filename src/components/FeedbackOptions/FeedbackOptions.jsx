56c02cc59cb9b7612d4aa59a58b01e7d35b79d03
Mykola <kolanuch13@gmail.com>
2023-01-11 16:18:38 +0200

    Add styles.

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import PropTypes from 'prop-types';
+import css from "./FeedbackOptions.module.css"
 
 const FeedbackOptions = ({good, neutral, bad}) => (
     <div>
-        <ul>
-            <li><button onClick={good} type="button">Good</button></li>
-            <li><button onClick={neutral} type="button">Neutral</button></li>
-            <li><button onClick={bad} type="button">Bad</button></li>
+        <ul className={css.FeedbackList}>
+            <li><button onClick={good} type="button" className={css.FeedbackButton}>Good</button></li>
+            <li><button onClick={neutral} type="button" className={css.FeedbackButton}>Neutral</button></li>
+            <li><button onClick={bad} type="button" className={css.FeedbackButton}>Bad</button></li>
         </ul>
     </div>
 )
--- src/components/Statistic/Statistic.jsx
+++ src/components/Statistic/Statistic.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from 'prop-types';
+import css from "./Statistic.module.css"
 
 const Statistic = ({good, neutral, bad, total, positivePercentage}) => (
     <div>
@@ -7,12 +8,12 @@ const Statistic = ({good, neutral, bad, total, positivePercentage}) => (
         {total === 0 ?
             <span>Thiere is no feedbacks</span>
             :
-            <ul>
-                <li><span>Good: {good}</span></li>
-                <li><span>Neutral: {neutral}</span></li>
-                <li><span>Bad: {bad}</span></li>
-                <li><span>Total: {total}</span></li>
-                <li><span>Positive feedback: {positivePercentage}% </span></li>
+            <ul className={css.StatisticList}>
+                <li className={css.StatisticItem}><span>Good: {good}</span></li>
+                <li className={css.StatisticItem}><span>Neutral: {neutral}</span></li>
+                <li className={css.StatisticItem}><span>Bad: {bad}</span></li>
+                <li className={css.StatisticItem}><span>Total: {total}</span></li>
+                <li className={css.StatisticItem}><span>Positive feedback: {positivePercentage}% </span></li>
             </ul>
         }
     </div>
